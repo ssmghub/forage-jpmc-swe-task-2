2254b26e5d181b73e0d48b31c5c9d5f1b02c126c
ssmghub <ssm_0820@163.com>
2024-07-05 18:23:07 +0100

    jp update 1.0

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+  //// ssm_JP: 1 change
+  showGraph: boolean
 }
 
 /**
@@ -22,6 +24,8 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      //// ssm_JP: 2 change
+      showGraph: false
     };
   }
 
@@ -29,18 +33,37 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
-    return (<Graph data={this.state.data}/>)
+    //// ssm_JP: 3 change
+    if (this.state.showGraph){
+      return (<Graph data={this.state.data}/>)
+    }
   }
 
   /**
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-    });
+    // DataStreamer.getData((serverResponds: ServerRespond[]) => {
+    //   // Update the state by creating a new array of data that consists of
+    //   // Previous data in the state and the new data from server
+    //   this.setState({ data: [...this.state.data, ...serverResponds] });
+    // });
+    //// ssm_JP: 4 change
+    let x = 0;
+    // 设置定时器：const interval = setInterval(() => { ... }, 100);
+    // 使用 setInterval 函数每隔 100 毫秒执行一次内部的回调函数。
+    const interval = setInterval(() => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        this.setState({ 
+          data: serverResponds,
+          showGraph: true,
+        });
+      });
+      x++;
+      if (x > 1000) {
+        clearInterval(interval);
+      }
+    }, 100); //使用 setInterval 函数每隔 100 毫秒执行一次内部的回调函数。
   }
 
   /**
